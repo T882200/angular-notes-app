c920b139b15b3ef8a8dabcc2b3526f46b8a52cbe
Idan Madar <t882200@gmail.com>
2018-12-29 23:27:40 +0000

    Fixed bugs in the deploy process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBBaMtskL65wOOhNY_uBLe-MZX4Ky0_4hI",
+    authDomain: "notes-app-801f7.firebaseapp.com",
+    databaseURL: "https://notes-app-801f7.firebaseio.com",
+    projectId: "notes-app-801f7",
+    storageBucket: "notes-app-801f7.appspot.com",
+    messagingSenderId: "417505057337"
+  }
 };
